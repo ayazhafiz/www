de9ee7e4399ab5e1bb94f326a6fb961bf41f4370
ayazhafiz <ayaz.hafiz.1@gmail.com>
2017-06-30 17:30:12 -0500

    :bug: macro redep

--- src/kemal/partials.cr
+++ src/kemal/partials.cr
@@ -8,12 +8,12 @@ module Partials
   LAYT = "./views/layouts"
 
   PARTIALS = {
-    meta:        "#{HEAD}/meta.ecr",
-    webapp:      "#{HEAD}/webapp.ecr",
-    mathjax:     "#{HEAD}/js/mathjax.ecr",
-    projects:    "#{INCL}/projects.ecr",
-    podd_sample: "#{INCL}/podd-sample.ecr",
-    whale:       "#{INCL}/whale.ecr",
-    index_head:  "#{HEAD}/_for-index.ecr",
+    meta:        "#{{{HEAD}}}/meta.ecr",
+    webapp:      "#{{{HEAD}}}/webapp.ecr",
+    mathjax:     "#{{{HEAD}}}/js/mathjax.ecr",
+    projects:    "#{{{INCL}}}/projects.ecr",
+    podd_sample: "#{{{INCL}}}/podd-sample.ecr",
+    whale:       "#{{{INCL}}}/whale.ecr",
+    index_head:  "#{{{HEAD}}}/_for-index.ecr",
   }
 end
