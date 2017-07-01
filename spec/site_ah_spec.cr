a22ff5119aaa21feb421f4d7b9c4f95510547757
ayazhafiz <ayaz.hafiz.1@gmail.com>
2017-06-30 21:24:49 -0500

    :white_check_mark: Test fixes

--- spec/site_ah_spec.cr
+++ spec/site_ah_spec.cr
@@ -22,8 +22,8 @@ describe "Vector API" do
 
   it "generates accurate 3D vector json" do
     get_vector_json(
-      vect_1: (Vector2D.from_json %({"i":1,"j":1,"k":1})),
-      vect_2: (Vector2D.from_json %({"i":1,"j":1,"k":1}))
+      vect_1: (Vector3D.from_json %({"i":1,"j":1,"k":1})),
+      vect_2: (Vector3D.from_json %({"i":1,"j":1,"k":1}))
     ).should eq %({"one":"<1, 1, 1>","two":"<1, 1, 1>","mag_one":1.7320508075688772,"mag_two":1.7320508075688772,"add":"<2, 2, 2>","sub":"0","dot":3.0,"cross":"0","angle_rad":0.0,"angle_deg":0.0})
   end
 end
