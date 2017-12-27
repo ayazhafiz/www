982323b7245fde804281f39b9167f7fdd1819a03
ayazhafiz <ayaz.hafiz.1@gmail.com>
2017-12-27 08:44:21 -0700

    construction: fixing Heroku build

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 KEMAL_ENV=$NODE_ENV
 
 if [ "$KEMAL_ENV" == "production" ]; then
+  . "$(pwd)/target/release/originaldir"
+  echo "$ORIGINAL_DIR"
+  mkdir -p "$ORIGINAL_DIR/src/rmath"
+  mkdir -p "$ORIGINAL_DIR/target/release"
+  cp ./target/release/librcalc.so "$ORIGINAL_DIR/target/release/librcalc.so"
+
   webpack -p
   KEMAL_ENV=$KEMAL_ENV ./app --port "$PORT"
 else
