82fa6ddc1867cb7ecf2204ef32d14de6ad151737
ayazhafiz <ayaz.hafiz.1@gmail.com>
2018-11-02 14:12:30 -0500

    fix script url

--- src/bin.cr
+++ src/bin.cr
@@ -3,11 +3,11 @@ require "kemal"
 # Returns a script or its installer, depending on whether the correct password
 # is provided.
 get "/bin/:script" do |env|
-  script = env.params.url["script"].upcase
+  script = env.params.url["script"]
   password = env.params.query["password"]?
 
   if password
-    if ENV["HAFIZ_#{script}_PASSWORD"] === password
+    if ENV["HAFIZ_#{script.upcase}_PASSWORD"] === password
       env.redirect ENV["HAFIZ_#{script}_URL"]
     else
       "echo 'Incorrect password!'"
