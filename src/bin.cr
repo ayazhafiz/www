f571898a9d2b5993001a3e2972a3b820da989fcc
ayazhafiz <ayaz.hafiz.1@gmail.com>
2018-11-02 13:59:44 -0500

    bin system

---
+++ src/bin.cr
@@ -0,0 +1,18 @@
+require "kemal"
+
+# Returns a script or its installer, depending on whether the correct password
+# is provided.
+get "/bin/:script" do |env|
+  script = env.params.url["script"].upcase
+  password = env.params.query["password"]?
+
+  if password
+    if ENV["HAFIZ_#{script}_PASSWORD"] === password
+      env.redirect ENV["HAFIZ_#{script}_URL"]
+    else
+      "echo 'Incorrect password!'"
+    end
+  else
+    env.redirect "/scripts/#{script}"
+  end
+end
