4f043751ce6e7e35d1a540efa5aa946cb341a7a9
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2019-01-01 09:32:04 -0600

    Update cmake_build.sh

--- cmake_build.sh
+++ cmake_build.sh
@@ -5,8 +5,15 @@ CMAKE_EXE=${CMAKE:=cmake}
 git clone --recursive https://github.com/ayazhafiz/sherpa_41 deps/sherpa_41
 cd deps/sherpa_41
 
-echo "Including ImageMagick dirs $MAGICK_PATH $MAGICK_BASE"
-$CMAKE_EXE -DImageMagick_INCLUDE_DIRS="$MAGICK_PATH;$MAGICK_BASE" .
+$CMAKE_EXE \
+  -DImageMagick_Magick++_INCLUDE_DIR:PATH="$MAGICK_BASE/include/ImageMagick-6" \
+  -DImageMagick_Magick++_LIBRARY:FILEPATH="$MAGICK_BASE/lib/libMagick++-6.Q16.so" \
+  -DImageMagick_MagickWand_INCLUDE_DIR:PATH="$MAGICK_BASE/include/ImageMagick-6" \
+  -DImageMagick_MagickWand_LIBRARY:FILEPATH="$MAGICK_BASE/lib/libMagickWand-6.Q16.so" \
+  -DImageMagick_MagickCore_INCLUDE_DIR:PATH="$MAGICK_BASE/include/ImageMagick-6" \
+  -DImageMagick_MagickCore_LIBRARY:FILEPATH="$MAGICK_BASE/lib/libMagickCore-6.Q16.so" \
+  .
+  
 make
 ./sherpa_41-test
 mv ./sherpa_41 ../../bin/sherpa_41
