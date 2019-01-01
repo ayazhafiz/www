5a0a5ab00c5887e6b245a0fc9156425406abd205
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-12-31 21:50:22 -0600

    Update cmake_build.sh

--- cmake_build.sh
+++ cmake_build.sh
@@ -4,7 +4,8 @@ CMAKE_EXE=${CMAKE:=cmake}
 
 echo "CXX is $CXX"
 git clone --recursive https://github.com/ayazhafiz/sherpa_41 deps/sherpa_41
-cd deps/sherpa_41 && $CMAKE_EXE .
+cd deps/sherpa_41
+CXX=$CXX $CMAKE_EXE .
 make
 ./sherpa_41-test
 mv ./sherpa_41 ../../bin/sherpa_41
