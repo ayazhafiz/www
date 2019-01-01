7650a5f449e0e3fc02d1ffe34b96969f60c1f46e
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2019-01-01 09:20:08 -0600

    Update cmake_build.sh

--- cmake_build.sh
+++ cmake_build.sh
@@ -2,10 +2,11 @@
 
 CMAKE_EXE=${CMAKE:=cmake}
 
-echo "CXX is $CXX"
 git clone --recursive https://github.com/ayazhafiz/sherpa_41 deps/sherpa_41
 cd deps/sherpa_41
-CXX="$CXX" $CMAKE_EXE .
+
+echo "Including ImageMagick dirs $MAGICK_PATH $MAGICK_BASE"
+$CMAKE_EXE -DImageMagick_INCLUDE_DIRS="$MAGICK_PATH;$MAGICK_BASE" .
 make
 ./sherpa_41-test
 mv ./sherpa_41 ../../bin/sherpa_41
