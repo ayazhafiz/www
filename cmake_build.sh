1c7cc5d72af40b47f209aac5d246298fa5564916
ayaz hafiz <ayaz.hafiz.1@gmail.com>
2018-12-31 22:56:48 -0600

    Update cmake_build.sh

--- cmake_build.sh
+++ cmake_build.sh
@@ -6,7 +6,7 @@ ls -l /app/vendor/imagemagick/include/ImageMagick-6
 echo "CXX is $CXX"
 git clone --recursive https://github.com/ayazhafiz/sherpa_41 deps/sherpa_41
 cd deps/sherpa_41
-CXX=$CXX $CMAKE_EXE .
+CXX="$CXX" $CMAKE_EXE .
 make
 ./sherpa_41-test
 mv ./sherpa_41 ../../bin/sherpa_41
